3ddc51388d076b662b31de5a91cd4d49fed8c8cd
aidoyuzuna <hoshipaso+gi@gmail.com>
2024-12-01 08:22:08 +0900

    ✨ datetimeを1つに統一・処理の簡略化

--- moon-transit.py
+++ moon-transit.py
@@ -14,44 +14,74 @@ mastodon = Mastodon(
 )
 
 
-def time_word_calc(now: datetime.time):
-    """今の時刻が午前か午後か調べる
-
+def determine_end_time(now: datetime.time) -> datetime:
+    """月星座の判定終了時刻を返す
     Args:
-        now (datetime.time): 今の時刻
-
+        now (datetime.time): 現在の時刻
     Returns:
-        datetime: 11:59もしくは23:59を返す
-        str: 午前もしくは午後
+        datetime: 判定が終わる時刻 (11:59または23:59)
     """
-    if now < datetime.time(12, 0, 0):
-        time = datetime.time(11, 59, 0)
-        word = "午前"
+
+    if now.hour < 12:
+        return datetime.datetime(now.year, now.month, now.day, 11, 59, 0)
 
     else:
-        time = datetime.time(23, 59, 0)
-        word = "午後"
-    return time, word
+        return datetime.datetime(now.year, now.month, now.day, 23, 59, 0)
 
 
-def export_mastodon(start: str, end: str, today: datetime.date, time: str):
+def create_post_message(start: str, end: str, today: datetime.date) -> str:
     """月星座をMastodonに出力
 
     Args:
         start (str): 現在の月星座
         end (str): 11:59もしくは23:59時点での月星座
-        today (datetime.date): 現在の日付
-        time (str): 現在の時刻
-    """
-    date = datetime.datetime.strftime(today, "%Y年%m月%d日")
-    posttext = ""
+        today (datetime.date): 現在の日時
 
-    if start == end:
-        posttext = (
-            f"{date}{time}の月星座ニュースです。\n今後12時間、{start}になります。"
+    Returns:
+        str: Mastodonに投稿する内容
+    """
+
+    date: str = datetime.datetime.strftime(today, "%Y年%m月%d日")
+    text: str = ""
+
+    if start == end and today.hour < 12:
+        text = (
+            f"{date}午前の月星座ニュースです。\n今後12時間、月星座は{start}になります。"
         )
+
+    elif start == end and 12 < today.hour:
+        text = (
+            f"{date}午後の月星座ニュースです。\n今後12時間、月星座は{start}になります。"
+        )
+
+    elif start != end and today.hour < 12:
+        text = f"{date}午前の月星座ニュースです。\n今後12時間以内に月星座が{start}から{end}に変わります。"
     else:
-        posttext = f"{date}{time}の月星座ニュースです。\n今後12時間以内に{start}から{end}に変わります。"
+        text = f"{date}午後の月星座ニュースです。\n今後12時間以内に月星座が{start}から{end}に変わります。"
+
+    return text
+
+
+def main():
+    # 日付・タイムゾーン入力
+    today_datetime: datetime = datetime.datetime.now()
+    timezone_offset: int = 9
+
+    end_time: datetime = determine_end_time(today_datetime)
+
+    # 月星座計算
+    start_moon: str = common_calc.calculate_planet_position(
+        common_calc.convert_to_julian_date(today_datetime, timezone_offset), 1
+    )
+
+    end_moon: str = common_calc.calculate_planet_position()
+
+    # 出力
+    posttext: str = create_post_message(
+        common_calc.determine_sign(start_moon),
+        common_calc.determine_sign(end_moon),
+        today_datetime,
+    )
 
     print(posttext)
 
@@ -59,32 +89,6 @@ def export_mastodon(start: str, end: str, today: datetime.date, time: str):
     #    status=posttext,
     #    visibility="public",
     # )
-
-
-def main():
-    # 日付・タイムゾーン入力
-    today_date = datetime.date.today()
-    now_time = datetime.datetime.now().time()
-    timezone_offset = 9
-
-    end_time, time_word = time_word_calc(now_time)
-
-    # 月星座計算
-    start_moon = common_calc.calculate_planet_position(
-        common_calc.convert_to_julian_date(today_date, now_time, timezone_offset), 1
-    )
-
-    end_moon = common_calc.calculate_planet_position(
-        common_calc.convert_to_julian_date(today_date, end_time, timezone_offset), 1
-    )
-
-    # 出力
-    export_mastodon(
-        common_calc.determine_sign(start_moon),
-        common_calc.determine_sign(end_moon),
-        today_date,
-        time_word,
-    )
 
 
 if __name__ == "__main__":
