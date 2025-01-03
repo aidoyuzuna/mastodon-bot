e5efd937dae068fb3c077f66c94b43bacefd3f76
aidoyuzuna <hoshipaso+gi@gmail.com>
2025-01-03 19:14:22 +0900

    ✨ astrology_dataに対応

--- astlogy-bot/today_astrodice.py
+++ astlogy-bot/today_astrodice.py
@@ -1,36 +1,7 @@
+import astrology_data
 import random
 import datetime
-import pyperclip
-
-# 星座
-sign: tuple[str] = (
-    "牡羊座",
-    "牡牛座",
-    "双子座",
-    "蟹座",
-    "獅子座",
-    "乙女座",
-    "天秤座",
-    "蠍座",
-    "射手座",
-    "山羊座",
-    "水瓶座",
-    "魚座",
-)
-
-# 惑星
-planet: tuple[str] = (
-    "太陽",
-    "月",
-    "水星",
-    "金星",
-    "火星",
-    "木星",
-    "土星",
-    "天王星",
-    "海王星",
-    "冥王星",
-)
+import locale
 
 
 def get_current_date():
@@ -62,9 +33,10 @@ def roll_dice():
         house_roll (int): ハウスのさいころ結果
         planet_roll (str): 惑星のさいころ結果
     """
-    sign_roll: str = random.choice(sign)
+    sign_roll: str = select_sign(random.randint(0, 11))
     house_roll: int = random.randint(1, 12)
-    planet_roll: str = random.choice(planet)
+    planet_roll: str = select_planet(random.randint(0, 9))
+
     return (
         sign_roll,
         house_roll,
@@ -72,17 +44,65 @@ def roll_dice():
     )
 
 
+def select_sign(select: int) -> str:
+    """ランダムの数値からサインを判定
+
+    Args:
+        select (int): ランダムの数値
+
+    Raises:
+        ValueError: selectが11以下の整数か確認
+        ValueError: selectの数値が星座番号と一致していないか確認
+
+    Returns:
+        str: 星座の名前
+    """
+    if select < 0 or select > 11:
+        raise ValueError(
+            f"select は11以下の整数である必要がある: {select}"
+        )  # エラーで原因が分かるようにする
+
+    for sign in astrology_data.ZodiacSign:
+        if sign.index == select:
+            return sign.sign_name
+    raise ValueError(f"{select} is not a valid ZodiacSign index")
+
+
+def select_planet(select: int) -> str:
+    """ランダムの数値から惑星を判定
+
+    Args:
+        select (int): ランダムの数値
+    Raises:
+        ValueError: selectが9以下の整数か確認
+        ValueError: selectの数値が星座番号と一致していないか確認
+
+    Returns:
+        str: 惑星の名前
+    """
+    if select < 0 or select > 9:
+        raise ValueError(
+            f"select は9以下の整数である必要がある: {select}"
+        )  # エラーで原因が分かるようにする
+
+    for sign in astrology_data.Planet:
+        if sign.index == select:
+            return sign.planet_name
+    raise ValueError(f"{select} is not a valid ZodiacSign index")
+
+
 def main():
-    # ランダム生成
-    generate_seed(get_current_date())
+    # 日付設定・ランダム生成
+    locale.setlocale(locale.LC_TIME, "ja_JP.UTF-8")
+    today = get_current_date()
+    generate_seed(today)
 
     # さいころを振る
     sign_choice, house_choice, planet_choice = roll_dice()
 
     # 結果を表示・コピー
-    result_message = f"おはよ～！\n\nサイン：{sign_choice}\nハウス：{house_choice}\n惑星：{planet_choice}\n"
+    result_message = f"{today:%Y年%m月%d日（%a）}の運勢だよ！\n\nサイン：{sign_choice}\nハウス：{house_choice}\n惑星：{planet_choice}\n"
     print(result_message)
-    pyperclip.copy(result_message)
 
 
 if __name__ == "__main__":
