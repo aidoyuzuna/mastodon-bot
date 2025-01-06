82bf83645285e1fc6d063c278dd10db4b0e9c424
aidoyuzuna <hoshipaso+gi@gmail.com>
2025-01-06 10:31:22 +0900

    ✨ Mastodonの読み込みをローカル・toot表示最適化

--- drink-water.py
+++ drink-water.py
@@ -3,24 +3,25 @@ from dotenv import load_dotenv
 import os
 from mastodon import Mastodon
 
-mastodon = Mastodon(
-    client_id=os.environ.get("DRINK_ID"),
-    client_secret=os.environ.get("DRINK_SECRET"),
-    access_token=os.environ.get("DRINK_TOKEN"),
-    api_base_url=os.environ.get("API_URL"),
-)
 
-
-def toot(time):
-    toot_text = f"{time}だよ！\nお水飲んで！ぐびー :ablobcat_drinkwater:"
+def toot(mastodon_api: Mastodon, time: datetime):
+    toot_text = f"{time :%H時%M分}だよ！お水飲んで！ぐびー :ablobcat_drinkwater:"
     print(toot_text)
-    mastodon.status_post(status=toot_text, visibility="public")
+    mastodon_api.status_post(status=toot_text, visibility="public")
 
 
 def main():
+    # 各種APIの読み込み
+    load_dotenv()
+    mastodon = Mastodon(
+        client_id=os.environ.get("DRINK_ID"),
+        client_secret=os.environ.get("DRINK_SECRET"),
+        access_token=os.environ.get("DRINK_TOKEN"),
+        api_base_url=os.environ.get("API_URL"),
+    )
+
     now = datetime.datetime.now().time()
-    now_format = now.strftime("%H時%M分")
-    toot(now_format)
+    toot(mastodon, now)
 
 
 if __name__ == "__main__":
