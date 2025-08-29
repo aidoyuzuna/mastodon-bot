cae713a4ee938609571cd2abef8f2874a059344f
aidoyuzuna <hoshipaso+gi@gmail.com>
2025-08-29 10:41:45 +0900

    ✨ #31 .envの環境変数チェックを実装

--- drink_water.py
+++ drink_water.py
@@ -1,7 +1,38 @@
 import datetime
 from dotenv import load_dotenv
 import os
-from mastodon import Mastodon
+import sys
+from mastodon import Mastodon, MastodonUnauthorizedError
+
+
+# 各種APIの読み込み
+def initialize_mastodon() -> Mastodon:
+    if not all(
+        [
+            os.environ.get("DRINK_ID"),
+            os.environ.get("DRINK_SECRET"),
+            os.environ.get("DRINK_TOKEN"),
+            os.environ.get("API_URL"),
+        ]
+    ):
+        raise KeyError("Mastodonの必要な環境変数が設定されていません")
+
+    mastodon_api = Mastodon(
+        client_id=os.environ.get("DRINK_ID"),
+        client_secret=os.environ.get("DRINK_SECRET"),
+        access_token=os.environ.get("DRINK_TOKEN"),
+        api_base_url=os.environ.get("API_URL"),
+    )
+
+    # 認証チェック
+    try:
+        mastodon_api.me()
+    except MastodonUnauthorizedError as e:
+        raise MastodonUnauthorizedError(
+            "認証エラー: アクセストークンが無効・もしくは読み込み権限がありません。"
+        ) from e
+
+    return mastodon_api
 
 
 def post_water_reminder(mastodon_api: Mastodon, time: datetime.time):
@@ -17,14 +48,9 @@ def post_water_reminder(mastodon_api: Mastodon, time: datetime.time):
 
 
 def main():
-    # 各種APIの読み込み
+    # 設定ファイル読み込み
     load_dotenv()
-    mastodon = Mastodon(
-        client_id=os.environ.get("DRINK_ID"),
-        client_secret=os.environ.get("DRINK_SECRET"),
-        access_token=os.environ.get("DRINK_TOKEN"),
-        api_base_url=os.environ.get("API_URL"),
-    )
+    mastodon = initialize_mastodon()
 
     now = datetime.datetime.now().time()
     post_water_reminder(mastodon, now)
